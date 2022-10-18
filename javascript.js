f21d1a73f4e80c6ad0cb306afa2b23f16aa97620
kyungminkim1 <kyungminkimnz@gmail.com>
2022-10-18 16:36:18 +1300

    Create pseudocode to add container div

--- javascript.js
+++ javascript.js
@@ -2,3 +2,8 @@
 // Create 16x16 (= 196) square divs using nested loop
 // Assign a class to each div within container
 // Append each div to container
+
+// Get reference to body
+// Create div
+// Assign class 'container' to div
+// Append div to body
