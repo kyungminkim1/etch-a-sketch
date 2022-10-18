0770dc3ad765f17bb71830938396717a2e6ddc2d
kyungminkim1 <kyungminkimnz@gmail.com>
2022-10-18 22:23:59 +1300

    Add createGrid function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
 // Create function createGrid()
-// Create 16x16 (= 196) square divs using nested loop
-// Assign a class to each div within container
-// Append each div to container
+function createGrid(){
+    // Create 16x16 (= 196) square divs using nested loop
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            let gridSquare = document.createElement('div');
+
+            // Assign a class to each div within container
+            gridSquare.classList.add('grid-square');
+            // Append each div to container
+            container.appendChild(gridSquare);
+            }
+        }
+}
+
 
 // Get reference to body
 const body = document.querySelector('body');
@@ -11,3 +22,5 @@ const container = document.createElement('div');
 container.classList.add('container');
 // Append div to body
 body.appendChild(container);
+
+createGrid();
